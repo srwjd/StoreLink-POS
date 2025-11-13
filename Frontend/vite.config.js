2ba49fb46d5f160dbbc244b85588e36e82e77da9
srwjd <jandangsirawan096@gmail.com>
2025-11-14 02:07:01 +0700

    1

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   server: {
     historyApiFallback: true, // ✅ ให้ redirect ทุก path กลับ index.html
